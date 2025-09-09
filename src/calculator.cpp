88a7b3c605216aecff37b212f979f982f310023f
bearcattt <ewigglet8@gmail.com>
2025-09-09 12:54:22 -0400

    Correct output statement in calculator.cpp
    Fixed syntax error in output statement.

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -2,5 +2,6 @@
 
 
 int main() {
- std::cout >> "Enter a basic math problem (ex. 5*5, 5+5)
+ std::cout >> "Enter a basic math problem (ex. 5*5, 5+5)";
+ return 0; // this means code ran with 0 problems appearentlu
 }
