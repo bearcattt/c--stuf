5a82c844843b5442c05d789ab9583e7418c421bb
bearcattt <ewigglet8@gmail.com>
2025-09-25 23:02:15 -0400

    add a spoice (space)

--- src/guessing.cpp
+++ src/guessing.cpp
@@ -13,6 +13,7 @@ int main() {
  player.attempts = 0;
  std::cout << "Enter level difficulty (1-3): ";
  std::cin >> player.difficulty; // stupid carrots smh
+ 
  if (player.difficulty > 3 || player.difficulty < 1) {
    std::cout << "The number you entered is not 1-3, stop being stubborn bro.\n";
    return 0;
