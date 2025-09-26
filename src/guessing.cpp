7aca6210eb1e0173e7f73c4630910b3f146747a1
bearcattt <ewigglet8@gmail.com>
2025-09-25 23:00:49 -0400

    okok

--- src/guessing.cpp
+++ src/guessing.cpp
@@ -1,8 +1,21 @@
+#include <string>
 #include <iostream>
 
-// am i supposted to do this class in main func???
+// update: im gud :D
 class plrInfo {
  public: 
- int attemps;
- int difficaulty;
+ int attempts;
+ int difficulty;
+};
+
+int main() {
+ plrInfo player; // class - object
+ player.attempts = 0;
+ std::cout << "Enter level difficulty (1-3): ";
+ std::cin >> player.difficulty; // stupid carrots smh
+ if (player.difficulty > 3 || player.difficulty < 1) {
+   std::cout << "The number you entered is not 1-3, stop being stubborn bro.\n";
+   return 0;
+ }
+ // when i get non lazy ill do this
 }
