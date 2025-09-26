bf485179063357f75832e1cb3eb3089c357308de
bearcattt <ewigglet8@gmail.com>
2025-09-26 12:26:53 -0400

    Add guess variable and input prompt for guessing game
    Added a guess variable to player info and prompt for user input.

--- src/guessing.cpp
+++ src/guessing.cpp
@@ -6,6 +6,7 @@ class plrInfo {
  public: 
  int attempts;
  int difficulty;
+ int guess;
 };
 
 int main() {
@@ -13,11 +14,13 @@ int main() {
  player.attempts = 0;
  std::cout << "Enter level difficulty (1-3): ";
  std::cin >> player.difficulty; // stupid carrots smh
- 
+ // TODO: add checking for letters or blank
  if (player.difficulty > 3 || player.difficulty < 1) {
    std::cout << "The number you entered is not 1-3, stop being stubborn bro.\n";
    return 0;
  }
+ std::cout << "Type a number 1-10: ";
+ std::cin >> player.guess
  // when i get non lazy ill do this
  return 0;
 }
